94ad71259b225e3c440aa7e2da7011c697d91888
Ewelina Fiedorowicz <ewelinaformella@gmail.com>
2020-03-27 19:32:02 +0100

    Reafactor in book tests

--- src/test/java/pl/fairit/somedayiwill/book/booksearch/GoogleBooksServiceTest.java
+++ src/test/java/pl/fairit/somedayiwill/book/booksearch/GoogleBooksServiceTest.java
@@ -12,11 +12,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 import pl.fairit.somedayiwill.book.testbooks.TestGBook;
 import pl.fairit.somedayiwill.book.testbooks.TestGBooks;
-import pl.fairit.somedayiwill.book.usersbooks.BookMapper;
-import pl.fairit.somedayiwill.book.usersbooks.Books;
-
-import java.util.Arrays;
-import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
@@ -24,10 +19,10 @@ import static org.mockito.Mockito.when;
 @ExtendWith(MockitoExtension.class)
 class GoogleBooksServiceTest {
     @Mock
-    RestTemplate restTemplate;
+    private RestTemplate restTemplate;
 
     @InjectMocks
-    GoogleBooksService booksService;
+    private GoogleBooksService booksService;
 
     @Test
     void shouldReturnBooksWhenAuthorGiven() {
