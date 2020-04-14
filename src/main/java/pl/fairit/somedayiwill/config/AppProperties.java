76991d3333e5dc760b261b84cb9c03d9175b2a00
Ewelina Fiedorowicz <ewelinaformella@gmail.com>
2020-04-14 15:06:26 +0200

    Add validation to @ApplicationProperties

--- src/main/java/pl/fairit/somedayiwill/config/AppProperties.java
+++ src/main/java/pl/fairit/somedayiwill/config/AppProperties.java
@@ -4,11 +4,16 @@ import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.client.RestTemplate;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Data
 @Configuration
 @ConfigurationProperties(prefix = "app")
+@Validated
 public class AppProperties {
     private final Auth auth = new Auth();
     private final Cron cron = new Cron();
@@ -22,25 +27,33 @@ public class AppProperties {
 
     @Data
     public static class Auth {
+        @NotBlank
         private String tokenSecret;
+        @NotNull
         private long tokenExpirationMills;
     }
 
     @Data
     private static class Cron {
+        @NotBlank
         private String weeklyPattern;
+        @NotBlank
         private String monthlyPattern;
     }
 
     @Data
     private static class GoogleBooks {
+        @NotBlank
         private String key;
+        @NotBlank
         private String baseUrl;
     }
 
     @Data
     private static class MovieDatabase {
+        @NotBlank
         private String key;
+        @NotBlank
         private String baseUrl;
     }
 }
